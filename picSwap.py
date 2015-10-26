ba8607945573e1beb3304dc62e0affe8b1934f05
OinkerBob <oinkerbob1@gmail.com>
2015-10-26 18:25:07 -0400

    Create picSwap.py
    v0.5
    --- Currently the client only creates necessary directories for storing the user's friends' public keys, as well as files containing the user's username and password, the user's private key, and an activity log for client debugging. 
    --- Socket creation is currently being developed, and the server (which is being hosted on my personal machine) will be up and running as soon as my home network infrastructure is configured properly. 
    --- Implementation of encryption has not been applied to the client. I'll need to work out compatibility issues with pyCrypto or find another method of encryption.

---
+++ picSwap.py
@@ -0,0 +1,86 @@
+#!usr/bin/env python
+######################################################################
+# "PicSwap" Encrypting File Sharing Client
+# AUTHOR: Brandon Denton
+#
+# THE FOLLOWING SCRIPT SERVES THE PURPOSE DESCRIBED BELOW. ITS AUTHOR
+# MAKES NO GUARANTEES OF ITS FITNESS FOR ANY OTHER PURPOSE. THE 
+# AUTHOR IS ALSO NOT RESPONSIBLE FOR ANY DATA TRANSMITTED VIA THE
+# SERVICE DESCRIBED BELOW. UTILIZE THE SERVICE AT YOUR OWN DISCRETION. 
+#
+# This script is the frontend for a general-purpose file sharing 
+# service that applies encryption in CBC mode to files it sends. The
+# script currently determines what files to send via user input to 
+# the command line and allows a user to "add friends", a process 
+# which queries the server for a desired username and distributes 
+# the public key associated with that name to the user for later 
+# decryption of files that desired user may send with their public 
+# key.
+######################################################################
+import socket
+import os
+from sys import platform as Platform
+import getpass    # We want to suppress user input of their password.
+import buildDirs as bd    # helper module that build's client's directory system
+
+def msgUpdate():
+    '''
+    This function, which executes at least once at runtime and 
+    also upon user request, queries the server for files that
+    the user's friends have delivered to the server to be sent
+    to the user. If any exist in the user's directory on the 
+    server, the server sends each file there to the user via
+    the open socket.
+    '''
+    print('idek')
+    
+
+def send():
+    '''
+    This function sends a specified file via the socket already
+    opened to my server in 1K chunks.
+    '''
+    fname = input('What file would you like me to send? ')
+    while(fname != "none" or fname != "I'm done." or fname != "done"):
+        fd = open(fname, 'r')
+        while True:    # Send the specified file via the socket.
+            data = clients.send(1024)    # buffering out 1K at a time
+            if not data: break
+
+
+## If the picSwap client doesn't have its own directory and ##
+## subdirectories for the user's friends' public keys and   ##
+## temporary files, make them. This functionality must be   ##
+## filesystem independent, hence the need for sys.platform. ##
+if(Platform == "win32"):
+    bd.makeWindirs()
+elif(Platform == "linux" or Platform == "darwin"):
+    bd.makeGNUdirs()
+
+## Now we must query the user for their username and password. ##
+name = input('username: ')
+pwd = getpass.getpass('password: ')
+if(Platform == "win32"):
+    os.chdir("C:/picSwap_data/account")
+	usr = open()
+	
+## Now that we've ensured we have a place to store needed   ##
+## files for the client, it's time to start allowing the    ##
+## user to send files to his or her friends. SOCKET TIME!!! ##
+host = 'myserver'    # need my broadcasting address here
+port = 24601    # This, like my server hostname, should hopefully never change.
+clients = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+clients.connect((host, port))
+
+msgUpdate()    # Always check for new files from friends at least once each time we run the client.
+
+while True:
+    if(input('What would you like to do? ') == 'send'):
+        send()
+    if(input('What would you like to do? ') == 'update'):
+        msgUpdate()
+    if(input('What would you like to do? ') == 'quit'):
+        if(input('Are you sure? y/n ') == 'y' or input('Are you sure? y/n') == 'yes'):
+            break    # The user is done. Close the client application.
+    else:
+        print("I don't understand. Please pick something else.")    
