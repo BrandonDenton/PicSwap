9e8448d16375d2bafda6494b4a27742d47a6cdab
OinkerBob <oinkerbob1@gmail.com>
2015-12-10 13:48:05 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-#!usr/bin/env python3
+#!usr/bin/env python
 ########################################################################
 # Stripped-down Snapchat Clone Server
 # AUTHOR: Brandon Denton
