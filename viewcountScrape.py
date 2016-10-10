61f8fbf042babba4b1fdc8ca5b65cffe41813648
BrandonDenton <oinkerbob1@gmail.com>
2016-10-10 14:56:21 -0400

    Add files via upload

---
+++ viewcountScrape.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import requests
+import requests.auth
+import json
+import threading
+import sys
+import pymongo
+
+def connectAndScrape():
+    ## Connection stuff
+    client = pymongo.MongoClient (host='da1.eecs.utk.edu')
+    db = client['D2Discovery']
+    twitchcoll = db.twitchstreams
+
+    print(twitchstreams.find_one({"game": "League of Legends"})
+
+
+if __name__ == '__main__':
+    connectAndScrape()
