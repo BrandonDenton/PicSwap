1147b2c28b258af9297a7afcafd728db2ff82756
BrandonDenton <oinkerbob1@gmail.com>
2016-10-15 16:20:59 -0400

    mongoDB connection timing out, need to figure out the proper port

--- scrapeViewCounts.py
+++ scrapeViewCounts.py
@@ -2,27 +2,57 @@
 import requests
 import requests.auth
 import json
+from bson.json_util import dumps as darkdump    # ばかな！
 import threading
 import sys
 import pymongo
+import csv
 
-def scrapeViews(gameName):
-	client = pymongo.MongoClient (host='da1.eecs.utk.edu')
-	db = client ['D2Discovery']
-	
-	view = db.twitchstreams.find({"streams": {"channel": {"game": gameName}}}, "viewers")
-	return(view)
-	
+'''This function flattens the multi-tiered JSON
+   objects returned from Twitch's API into a 
+   single-layered object that's fit for a CSV
+   file.'''
+def flatten4csv(input):
+    out = {}
+
+    def flatten(x, name=''):
+        if type(x) is dict:
+            for a in x:
+                flatten(x[a], name + a + '_')
+        elif type(x) is list:
+            i = 0
+            for a in x:
+                flatten(a, name + str(i) + '_')
+                i += 1
+        else:
+            out[name[:-1]] = x
+
+    flatten(input)
+    return out
+
+def connectAndScrape(gameName):
+    try:
+        client = pymongo.MongoClient (host='da1.eecs.utk.edu', port=9109)
+        db = client['D2Discovery']
+        twitchstreams = db.twitchstreams
+    except pymongo.errors.ServerSelectionTimeoutError:
+        print("Error: Unable to connect to da1")
+        twitchstreams = None
+    
+    if twitchstreams is not None:
+        objflat = twitchstreams.find_one()
+        #objflat = flatten4csv(db.twitchstreams.find({"streams": {"game": gameName}}))
+        return(objflat)
+    
 def queryGames():
-	## Open the config file and build game queries from its contents. ##
+    ## Open the config file and build game queries from its contents. ##
     viewsOfGames = []
-	gamefile = open("gamelist.txt", 'r')
-	graphfile = open("graphfile.txt", 'wb')
-	
-	for i in gamefile:
-		viewsOfGames[i] = scrapeViews(i)
-		
-	return(viewsOfGames)
-	
+    gamefile = open("gamelist.txt", 'r')
+    graphfile = open("graphfile.txt", 'wb')
+
+    #for name in gamefile:
+    #   print(connectAndScrape(name))
+    connectAndScrape(viewsOfGames)
+     
 if __name__ == '__main__':
     queryGames()
